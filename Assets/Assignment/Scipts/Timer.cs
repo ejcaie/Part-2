d397effe51ee6650ebcb1f6554f202930a342f4d
evanc <evancaie705@gmail.com>
2024-02-25 18:05:10 -0500

    Added functionality to the slider.

---
+++ Assets/Assignment/Scipts/Timer.cs
@@ -0,0 +1,25 @@
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    public Slider slider;
+    public float timer;
+    void Start()
+    {
+        slider.value = 0;
+    }
+
+    void FixedUpdate()
+    {
+        slider.value += (Time.deltaTime * 0.1f);
+        timer = slider.value;
+        if (timer == 1)
+        {
+            SendMessage("LoadMenuScene", SendMessageOptions.DontRequireReceiver);
+        }
+    }
+}
